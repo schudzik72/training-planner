207989c88ec535bb42cec42be5d93a28e7f17ea3
Sławomir Chudzik <schudzik72@gmail.com>
2018-03-31 13:34:58 +0200

    Added new data mocks for data service

--- client/test/lib/mockData.js
+++ client/test/lib/mockData.js
@@ -8,7 +8,6 @@ var mockData = (function() {
         getWorkoutParameters: getWorkoutParameters,
         getExerciseTypes: getExerciseTypes,
         getExercises: getExercises,
-        getBodyParts: getBodyParts,
     };
 
     function getWorkouts() {
@@ -41,23 +40,110 @@ var mockData = (function() {
     }
 
     function getWorkoutExercises() {
-
+        return {
+            status: 'success',
+            data: [
+                {
+                    id: 1,
+                    name: 'Chest press',
+                    description: 'test description',
+                    exerciseTypeId: 1,
+                    bodyPartsEngaged: 'CHEST,ABS,TRICEPS',
+                    linkToExercise: 'http://abc.com',
+                    workoutId: 1,
+                },
+                {
+                    id: 2,
+                    name: 'Dips',
+                    description: 'test description',
+                    exerciseTypeId: 2,
+                    bodyPartsEngaged: 'ABS,CHEST,SHOULDER,TRICEPS',
+                    linkToExercise: 'http://abc.com',
+                    workoutId: 1,
+                },
+                
+            ]
+        };
     }
 
     function getWorkoutParameters() {
-
+        return {
+            status: 'success',
+            data: [
+                {
+                    id: 1,
+                    name: 'Sets',
+                    value: '8-12',
+                    workoutId: 1
+                },
+                {
+                    id: 2,
+                    name: 'Repetitions',
+                    value: '3-4',
+                    workoutId: 1
+                }
+            ]
+        };
     }
 
     function getExerciseTypes() {
-
+        return {
+            status: 'success',
+            data: [
+                {
+                    id: 1,
+                    type: 'PUSH'
+                },
+                {
+                    id: 2,
+                    type: 'BODYWEIGHT'
+                }
+            ]
+        };
     }
 
     function getExercises() {
-        
-    }
-    
-    function getBodyParts() {
-
+        return {
+            status: 'success',
+            data: [
+                {
+                    id: 1,
+                    name: 'Chest press',
+                    description: 'test description',
+                    exerciseTypeId: 1,
+                    bodyPartsEngaged: 'CHEST,ABS,TRICEPS',
+                    linkToExercise: 'http://abc.com',
+                    workoutId: 1,
+                },
+                {
+                    id: 2,
+                    name: 'Dips',
+                    description: 'test description',
+                    exerciseTypeId: 2,
+                    bodyPartsEngaged: 'ABS,CHEST,SHOULDER,TRICEPS',
+                    linkToExercise: 'http://abc.com',
+                    workoutId: 1,
+                },
+                {
+                    id: 3,
+                    name: 'Pull ups',
+                    description: '',
+                    exerciseTypeId: 2,
+                    bodyPartsEngaged: 'ABS,TRICEPS,UPPER BACK,LOWER BACK',
+                    linkToExercise: 'http://abc.com',
+                    workoutId: 1,
+                },
+                {
+                    id: 4,
+                    name: 'Chin ups',
+                    description: '',
+                    exerciseTypeId: 2,
+                    bodyPartsEngaged: 'ABS,BICEPS,TRICEPS,UPPER BACK,LOWER BACK',
+                    linkToExercise: 'http://abc.com',
+                    workoutId: 1,
+                },
+            ]
+        };
     }
 
 })();
